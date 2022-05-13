99b519f6258dabaac3397da90d5edc1903254d2e
Rose Cota <rosejcota@gmail.com>
2022-05-13 10:11:28 -0500

    update page html in grid layout

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -1,5 +1,38 @@
 export const Exomine = () => {
   return `
-        <h1>Solar System Mining Marketplace</h1>
-        `;
+    <div class="container">
+      <h2>Solar System Mining Marketplace</h2>
+
+      <div class="row">
+        <div class="col-5">
+          <div class="row">
+            <div class="col-12">
+              <p>Choose Governor Placeholder</p>
+            </div>
+          </div><!-- row -->
+
+          <div class="row">
+            <div class="col-12">
+              <p>Choose Facility Placeholder</p>
+            </div>
+          </div><!-- row -->
+        </div><!-- col -->
+
+        <div class="col-7">
+          <h3>Colony Minerals</h3>
+        </div>
+      </div><!-- row -->
+      
+
+      <div class="row">
+        <div class="col-9">
+          <h4>Facility Minerals</h4>
+        </div>
+
+        <div class="col-3">
+          <h4>Cart</h4>
+        </div>
+      </div><!-- row -->
+    </div><!-- container -->
+  `;
 };

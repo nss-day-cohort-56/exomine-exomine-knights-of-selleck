c5c8364bb9118254d2d83178bc0ddd6e36c136eb
Dan Purdon <danpurdon@hotmail.com>
2022-05-17 14:46:13 -0500

    added purchase button to html

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -36,6 +36,7 @@ export const Exomine = () => {
 
         <div class="col-3">
           <h3 class="text-center">Cart</h3>
+          <button id ="orderButton">Purchase</button>
         </div>
       </div><!-- row -->
     </div><!-- container -->
